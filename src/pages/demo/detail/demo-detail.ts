647bb9f8980547366fa76656cae48e3cc58d3ccb
Limeiyuan <ojwftded@gmail.com>
2017-10-27 17:10:21 +0800

    案例详情demo（未完成）

--- src/pages/demo/detail/demo-detail.ts
+++ src/pages/demo/detail/demo-detail.ts
@@ -11,9 +11,37 @@ import {DemoService} from "../../../providers/demo-service-rest";
 })
 export class DemoDetailPage {
 
-  properties: Array<any>;
+  items: Array<any>;
+  currentId;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: DemoService, public config: Config) {
     console.log(navParams.data);
+    this.currentId = this.navParams.get("id");
+    this.query();
+  }
+
+  query(callback = null) {
+    this.service.queryDetail(this.currentId)
+      .then(resp => {
+        console.log(resp.result);
+        this.items = this.items.concat(resp.result);
+        if (callback) {
+          callback();
+        }
+      })
+      .catch(error => console.log(error));
+    console.log(this.items);
+  }
+
+  queryNext(callback = null) {
+    this.service.queryDetail(this.currentId)
+      .then(resp => {
+        this.items = this.items.concat(resp.result);
+        if (callback) {
+          callback();
+        }
+      })
+      .catch(error => console.log(error));
+    console.log(this.items);
   }
 }
